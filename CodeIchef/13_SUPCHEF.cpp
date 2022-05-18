fe57b921b4cb0d4132b7814c5a7846d6dd401eb2
Tokiosman80F <Tokiosman80F@users.noreply.github.com>
2022-05-18 12:50:11 +0600

    2 basic prblm add

---
+++ CodeIchef/13_SUPCHEF.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int testCase;
+	cin>>testCase;
+	while(testCase--)
+	{
+		int m,n,k;
+		cin>>m>>n>>k;
+		if (n*k<m)
+		{
+			cout<<"YES"<<endl;
+		}
+		else{
+
+			cout<<"NO"<<endl;
+			
+		}
+	}	
+}
---
+++ CodeIchef/14_VOWELTB.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	
+	char letter;
+	cin>>letter;
+	switch(letter)
+	{
+		case 'A':
+		cout<<"Vowel"<<endl;
+		break;
+
+		case 'E':
+		cout<<"Vowel"<<endl;
+		break;
+			case 'I':
+		cout<<"Vowel"<<endl;
+		break;
+			case 'O':
+		cout<<"Vowel"<<endl;
+		break;
+			case 'U':
+		cout<<"Vowel"<<endl;
+		break;
+		default:
+		cout<<"Consonant"<<endl;
+
+
+	}	
+}
